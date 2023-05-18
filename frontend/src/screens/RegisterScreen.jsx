f4467e3242af253c3d6edc3730de9b851b9953a0
Div Loop <80190976+thedivloop@users.noreply.github.com>
2023-05-18 14:15:33 +0700

    added homescreen, login screen and register screen

---
+++ frontend/src/screens/RegisterScreen.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+
+const RegisterScreen = () => {
+	const [email, setEmail] = useState("");
+	const [name, setName] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+
+	const submitHandler = async (e) => {
+		e.preventDefault();
+		console.log("submit");
+	};
+
+	return (
+		<FormContainer>
+			<h1>Sign Up</h1>
+			<Form onSubmit={submitHandler}>
+				<Form.Group className="my-2" controlId="name">
+					<Form.Label>Name</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Enter name"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+				<Form.Group className="my-2" controlId="email">
+					<Form.Label>Email Address</Form.Label>
+					<Form.Control
+						type="email"
+						placeholder="Enter Email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+				<Form.Group className="my-2" controlId="password">
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						type="password"
+						placeholder="Enter Password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+				<Form.Group className="my-2" controlId="confirmPassword">
+					<Form.Label>Confirm Password</Form.Label>
+					<Form.Control
+						type="password"
+						placeholder="Confirm Password"
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
+					></Form.Control>
+				</Form.Group>
+				<Button type="submit" variant="primary" className="mt-3">
+					Sign in
+				</Button>
+				<Row className="py-3">
+					<Col>
+						Existing Customer? <Link to="/login">Sign in</Link>
+					</Col>
+				</Row>
+			</Form>
+		</FormContainer>
+	);
+};
+
+export default RegisterScreen;
