4e3a1be1ffebb768c9dd5931f43553a5ef5bd28a
Div Loop <80190976+thedivloop@users.noreply.github.com>
2023-05-21 22:44:13 +0700

    modified hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -10,9 +10,9 @@ const Hero = () => {
 				<Card className="p-5 d-flex flex-column align-items-center hero-card bg-light w-75">
 					<h1 className="text-center mb-4">MERN Authentication</h1>
 					<p className="text-center mb-4">
-						This is a my own boilerplate for MERN authentication that stores a
-						JWT in an HTTP-Only cookie. It also uses Redux Toolkit and the React
-						Bootstrap library.
+						This is a beautiful boilerplate for MERN authentication that stores
+						a JWT in an HTTP-Only cookie. It also uses Redux Toolkit and the
+						React Bootstrap library.
 					</p>
 					<div className="d-flex">
 						<LinkContainer to="/login">
